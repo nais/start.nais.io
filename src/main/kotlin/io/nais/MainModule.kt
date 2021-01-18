452847d4e510fe5caac892a56d0fd74219881bc7
J-K. Solbakken <jksolbakken@gmail.com>
2021-01-18 15:32:25 +0100

    related module and routing in the same file

--- src/main/kotlin/io/nais/MainModule.kt
+++ src/main/kotlin/io/nais/MainModule.kt
@@ -4,12 +4,24 @@ import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.http.HttpStatusCode.Companion.BadRequest
+import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.http.content.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.serialization.*
+import io.nais.deploy.serialize
+import io.nais.mapping.appVarsFrom
+import io.nais.mapping.gitHubWorkflowFrom
+import io.nais.mapping.naisApplicationFrom
+import io.nais.metrics.Metrics
+import io.nais.naisapp.Environment
+import io.nais.naisapp.serialize
+import io.nais.request.Request
+import io.nais.zip.zipTo
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerializationException
+import java.nio.file.Paths
 
 @ExperimentalSerializationApi
 @Suppress("unused") // referenced in application.conf
@@ -35,5 +47,22 @@ fun Application.mainModule() {
       }
 
       app()
+   }
+}
+
+@ExperimentalSerializationApi
+fun Route.app() {
+   post("/app") {
+      val request = call.receive<Request>()
+      Metrics.countNewDownload(request.team, request.platform)
+      call.response.header(HttpHeaders.ContentDisposition, "attachment; filename=${request.appName}.zip")
+      call.respondOutputStream(ContentType.Application.Zip, OK) {
+         zipTo(this, mapOf(
+            Paths.get(".nais/nais.yaml") to naisApplicationFrom(request).serialize(),
+            Paths.get(".nais/dev.yaml") to appVarsFrom(request, Environment.DEV).serialize(),
+            Paths.get(".nais/prod.yaml") to appVarsFrom(request, Environment.PROD).serialize(),
+            Paths.get(".github/workflows/main.yaml") to gitHubWorkflowFrom(request).serialize(),
+         ))
+      }
    }
 }
