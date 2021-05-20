793c4eb7e3bd8293ba726fe94a6d9ee1b6ca3d37
J-K. Solbakken <jksolbakken@gmail.com>
2021-05-20 14:14:36 +0200

    where we're going, we don't need webservers

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,11 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import se.patrikerdes.UseLatestVersionsTask
 
-val ktorVersion = "1.5.4"
 val logbackVersion = "1.3.0-alpha5"
 val logstashEncoderVersion = "6.6"
-val mustacheVersion = "0.9.7"
 val junitJupiterVersion = "5.8.0-M1"
 val kotlinSerializationVersion = "1.2.0"
 val kamlVersion = "0.31.0"
-val micrometerVersion = "1.6.6"
 
 val mainClassName = "io.nais.MainKt"
 
@@ -30,18 +27,12 @@ repositories {
 
 dependencies {
    implementation(kotlin("stdlib"))
-   implementation("io.ktor:ktor-server-core:$ktorVersion")
-   implementation("io.ktor:ktor-server-netty:$ktorVersion")
-   implementation("io.ktor:ktor-serialization:$ktorVersion")
    implementation("ch.qos.logback:logback-classic:$logbackVersion")
    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$kotlinSerializationVersion")
    implementation("com.charleskorn.kaml:kaml:$kamlVersion")
-   implementation("io.ktor:ktor-metrics-micrometer:$ktorVersion")
-   implementation("io.micrometer:micrometer-registry-prometheus:$micrometerVersion")
 
    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-   testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
 
--- src/main/kotlin/io/nais/Main.kt
+++ src/main/kotlin/io/nais/Main.kt
@@ -1,3 +1,3 @@
 package io.nais
 
-fun main(): Unit = io.ktor.server.netty.EngineMain.main(emptyArray())
+fun main(): Unit = println("yo, sup?")
