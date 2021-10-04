6b2a9b8f85531ad60af3a1f3a48c95916e7fc717
J-K. Solbakken <jksolbakken@gmail.com>
2021-10-04 08:56:58 +0200

    upped dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,18 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import se.patrikerdes.UseLatestVersionsTask
 
-val ktorVersion = "1.6.2"
+val ktorVersion = "1.6.4"
 val logbackVersion = "1.3.0-alpha10"
 val logstashEncoderVersion = "6.6"
-val junitJupiterVersion = "5.8.0-RC1"
-val kotlinSerializationVersion = "1.2.2"
-val kamlVersion = "0.35.2"
-val micrometerVersion = "1.7.3"
+val junitJupiterVersion = "5.8.1"
+val kotlinSerializationVersion = "1.3.0"
+val kamlVersion = "0.36.0"
+val micrometerVersion = "1.7.4"
 
 val mainClassName = "io.nais.MainKt"
 
 plugins {
-   kotlin("jvm") version "1.5.21"
+   kotlin("jvm") version "1.6.0-M1"
    kotlin("plugin.serialization") version "1.5.21"
    id("se.patrikerdes.use-latest-versions") version "0.2.17"
    id("com.github.ben-manes.versions") version "0.39.0"
@@ -79,7 +79,7 @@ tasks {
    }
 
    withType<Wrapper> {
-      gradleVersion = "7.1"
+      gradleVersion = "7.2"
    }
 
    named("useLatestVersions", UseLatestVersionsTask::class.java).configure {
