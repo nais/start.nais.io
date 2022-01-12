b73388fa6ed019b841c72683feece12ebc88944f
J-K. Solbakken <jksolbakken@gmail.com>
2022-01-12 19:36:13 +0100

    upped deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,21 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
 import se.patrikerdes.UseLatestVersionsTask
 
 val ktorVersion = "1.6.5"
-val logbackVersion = "1.3.0-alpha10"
-val logstashEncoderVersion = "7.0"
-val junitJupiterVersion = "5.8.1"
-val kotlinSerializationVersion = "1.3.1"
-val kamlVersion = "0.37.0"
-val micrometerVersion = "1.8.0"
+val logbackVersion = "1.3.0-alpha12"
+val logstashEncoderVersion = "7.0.1"
+val junitJupiterVersion = "5.8.2"
+val kotlinSerializationVersion = "1.3.2"
+val kamlVersion = "0.38.0"
+val micrometerVersion = "1.8.1"
 
 val mainClassName = "io.nais.MainKt"
 
 plugins {
-   kotlin("jvm") version "1.6.0"
-   kotlin("plugin.serialization") version "1.6.0"
-   id("com.github.johnrengelman.shadow") version "7.1.0"
+   kotlin("jvm") version "1.6.10"
+   kotlin("plugin.serialization") version "1.6.10"
+   id("com.github.johnrengelman.shadow") version "7.1.2"
    id("se.patrikerdes.use-latest-versions") version "0.2.18"
-   id("com.github.ben-manes.versions") version "0.39.0"
+   id("com.github.ben-manes.versions") version "0.40.0"
 }
 
 java {
