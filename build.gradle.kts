56d3ef58d2a50daf282cf5a1e8984ede6dd28ed7
J-K. Solbakken <jksolbakken@gmail.com>
2022-04-08 13:20:33 +0200

    chore: upped dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 val ktorVersion = "2.0.0-eap-256"
-val logbackVersion = "1.3.0-alpha12"
+val logbackVersion = "1.3.0-alpha14"
 val logstashEncoderVersion = "7.0.1"
 val junitJupiterVersion = "5.8.2"
-val kamlVersion = "0.38.0"
-val micrometerVersion = "1.8.1"
+val kamlVersion = "0.43.0"
+val micrometerVersion = "1.8.4"
 
 val mainClassName = "io.nais.MainKt"
 
 plugins {
    kotlin("jvm") version "1.6.10"
-   kotlin("plugin.serialization") version "1.6.10"
+   kotlin("plugin.serialization") version "1.6.20"
    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
