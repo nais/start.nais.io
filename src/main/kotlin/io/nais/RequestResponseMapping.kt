308afd4e2d87ce7c34c3135e51e47de7fb94bcf4
J-K. Solbakken <jksolbakken@gmail.com>
2022-06-30 08:18:15 +0200

    removed unnecessary visibility modifier

--- src/main/kotlin/io/nais/RequestResponseMapping.kt
+++ src/main/kotlin/io/nais/RequestResponseMapping.kt
@@ -39,7 +39,7 @@ fun Map<String, String>.b64EncodeValues() = mapValues { it.value.base64Encode() 
  * The returned map preserves the entry iteration order of the original map.
  * Those entries of another [map] that are missing in this map are iterated in the end in the order of that [map].
  */
-public operator fun <K, V> Map<out K, V>.plus(map: Map<out K, V>): Map<K, V> =
+operator fun <K, V> Map<out K, V>.plus(map: Map<out K, V>): Map<K, V> =
    LinkedHashMap(this).apply { putAll(map) }
 
 private val generationLabels = mapOf(
